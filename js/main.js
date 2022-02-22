2d9835030d645dc0519a49407a2bd65a5b40aefb
Semen Bushmanov <bush.sem@mail.ru>
2022-02-22 11:21:20 +0500

    Simplify functions

--- js/main.js
+++ js/main.js
@@ -1,27 +1,11 @@
 function getRandomNumber (min, max) {
-  min = Math.ceil(Math.abs(min));
-  max = Math.floor(Math.abs(max));
-  if (min > max) {
-    const swap = min;
-    min = max;
-    max = swap;
-  }
-  if (min === max) {
-    return min;
-  }
-
   return Math.floor(Math.random() * (max-min+1)) + min;
 }
 
 getRandomNumber (55, 70);
 
 function commentLengthCheck (commentString, maxLength) {
-  const commentLength = String(commentString).length;
-  if (commentLength > maxLength) {
-    return false;
-  }
-
-  return true;
+  return commentString.length <= maxLength;
 }
 
 commentLengthCheck ('Some comment', 15);
