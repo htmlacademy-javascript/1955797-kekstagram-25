33850434065176c8a5398dd22dfa7d97aa395e63
Semen Bushmanov <bush.sem@mail.ru>
2022-04-08 13:11:44 +0500

    add api module to get and send data

---
+++ js/api.js
@@ -0,0 +1,32 @@
+import { showDownloadErrorMessage } from './messages.js';
+
+const getData = (onSuccess) => {
+  fetch('https://25.javascript.pages.academy/kekstagram/data')
+    .then((response) => response.json())
+    .then((photos) => {
+      onSuccess(photos);
+    })
+    .catch(showDownloadErrorMessage);
+};
+
+const sendData = (onSuccess, onFail, body) => {
+  fetch(
+    'https://25.javascript.pages.academy/kekstagram',
+    {
+      method: 'POST',
+      body,
+    },
+  )
+    .then((response) => {
+      if (response.ok) {
+        onSuccess();
+      } else {
+        onFail();
+      }
+    })
+    .catch(() => {
+      onFail();
+    });
+};
+
+export { getData, sendData };
