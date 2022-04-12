2656c35344898b86b4ac4d48c5431cd808b2fd2c
Semen Bushmanov <bush.sem@mail.ru>
2022-04-12 13:33:01 +0500

    delete unnecessary functions

--- js/gallery.js
+++ js/gallery.js
@@ -1,11 +1,8 @@
-// import { createPhotos } from './data.js';
 import { renderPictures } from './thumbnail.js';
 import { getData } from './api.js';
 
 const showGallery = () => {
   getData(renderPictures);
-//  const photos = createPhotos(25);
-//  renderPictures(photos);
 };
 
 export { showGallery };
--- js/util.js
+++ js/util.js
@@ -1,6 +1,5 @@
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 const getRandomItem = (items) => items[getRandomNumber(0, items.length - 1)];
-const checkLength = (string, maxLength) => string.length <= maxLength;
 const isEscapeKey =  (evt) => evt.key === 'Escape';
 
-export { getRandomNumber, checkLength, getRandomItem, isEscapeKey };
+export { getRandomNumber, getRandomItem, isEscapeKey };
